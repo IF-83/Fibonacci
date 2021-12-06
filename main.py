ccfda91f0d328a774cf630b4a568da22a6119698
IF <illesf16@gmail.com>
2021-12-07 00:53:19 +0100

    Recursive function call added to main for testing.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-
-
+import time
+import fibo_functions
 
 def main():
-    print("Testing Fibonacci algorithms.")
+    print("Testing Fibonacci algorithms:\n")
+
+    print("Recursive algorithm:")
+    n = 37
+    current_time = time.time()
+    print(f"For n = {n}: {fibo_functions.fibo_recursive(n)} - Execution time: {round(time.time() -  current_time, 3)} sec.")
+
+
 
 
 if __name__ == "__main__":
