0ba15b41ec2449fec8c6ef605c205ea2c92d64e2
IF <illesf16@gmail.com>
2021-12-09 00:40:47 +0100

    main file modified to display large numbers in scientific format and call for the sequential array algorithm added.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     start_time = time.time()
     f = fibo_functions.fibo_recursive(n)
     end_time = time.time()
-    print(f"For n = {n}: {f} - Execution time: {round(end_time -  start_time, 3)} sec.")
+    print(f"For n = {n}: {format_very_big_numbers(f)} - Execution time: {round(end_time -  start_time, 3)} sec.")
 
     print("\nRecursive algorithm with memoization:")
     n = 996
@@ -18,28 +18,38 @@ def main():
     fibo_memoization = fibo_classes.Fibo_Memoization()
     f = fibo_memoization(n)
     end_time = time.time()
-    if n > 100:
-        f = float(f)
-    print(f"For n = {n}: {f} - Execution time: {round(end_time -  start_time, 3)} sec.")
+    print(f"For n = {n}: {format_very_big_numbers(f)} - Execution time: {round(end_time -  start_time, 3)} sec.")
 
     print("\nRecursive algorithm with one recursive call:")
     n = 996
     start_time = time.time()
     f = fibo_functions.fibo_recursive_linear(n)
     end_time = time.time()
-    if n > 100:
-        f = float(f)
-    print(f"For n = {n}: {f} - Execution time: {round(end_time -  start_time, 3)} sec.")
+    print(f"For n = {n}: {format_very_big_numbers(f)} - Execution time: {round(end_time -  start_time, 3)} sec.")
 
     print("\nTail-call optimizable recursive algorithm:")
     n = 900
     start_time = time.time()
     f = fibo_functions.fibo_tail_call(n)
     end_time = time.time()
-    if n > 100:
-        f = float(f)
-    print(f"For n = {n}: {f} - Execution time: {round(end_time -  start_time, 3)} sec.")
+    print(f"For n = {n}: {format_very_big_numbers(f)} - Execution time: {round(end_time -  start_time, 3)} sec.")
 
+    print("\nSequential calculation and storing in array:")
+    n = 200000
+    start_time = time.time()
+    f = fibo_functions.fibo_array(n)    
+    end_time = time.time()
+    print(f"For n = {n}: {format_very_big_numbers(f)} - Execution time: {round(end_time -  start_time, 3)} sec.")
+
+def format_very_big_numbers(n: int) -> str:
+    if n <= 10**20:
+        return str(n)
+    k = 0
+    while n >= 10**17:
+        n //= 10
+        k += 1
+    x = float(n) / 10**16
+    return str(x) + "e+" + str(k + 16)
 
 
 if __name__ == "__main__":
