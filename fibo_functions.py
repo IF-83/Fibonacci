0cb1535c0213f9bd66260e5b6d50f99ffde18cd6
IF <illesf16@gmail.com>
2021-12-08 20:58:43 +0100

    Recursive algorithm with one recursive call. Returns 2 consecutive Fibo numbers.

--- fibo_functions.py
+++ fibo_functions.py
@@ -5,3 +5,21 @@ def fibo_recursive(n: int) -> int:
     if n <= 1:
         return n
     return fibo_recursive(n-1) + fibo_recursive(n-2)
+
+
+def __fibos_recursion(n: int) -> int:
+    """ auxiliary function """
+    if n <= 0:
+        raise Exception("Invalid argument: " + n)
+    if n == 1:
+        return [0,1]
+    prev_fib = __fibos_recursion(n-1)
+    return [prev_fib[1], sum(prev_fib)]
+
+
+def fibo_recursive_linear(n: int) -> int:
+    """ number of recursive calls is O(n); overall complexity: O(n^2) """
+    if n == 0:
+        return 0
+    return __fibos_recursion[1]
+
