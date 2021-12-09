5ff2c71075047e5d59c6f64109479217a56d00eb
IF <illesf16@gmail.com>
2021-12-09 21:19:31 +0100

    Class created for 2X2 matrices.

--- fibo_classes.py
+++ fibo_classes.py
@@ -8,3 +8,21 @@ class Fibo_Memoization:
             return self.memory[n]
         self.memory.append(self.memory[-1] + self.memory[-2])
         return self.__call__(n)
+
+
+class Mtx_2_times_2:
+    a, b, c, d = 0, 0, 0, 0  #columnwise
+    
+    # |a, c|
+    # |b, d|
+
+    def __init__(self, a, b, c, d) -> None:
+        self.a, self.b, self.c, self.d = a, b, c, d
+        return 
+
+    def __str__(self) -> str:
+        na, nb, nc, nd = len(str(self.a)), len(str(self.b)), len(str(self.c)), len(str(self.d)) 
+        nm = max([na,nb,nc,nd])
+        return f"| {' '*(nm - na) + str(self.a)}, {' '*(nm - nc) + str(self.c)} |\n| {' '*(nm - nb) + str(self.b)}, {' '*(nm - nd) + str(self.d)} |" 
+    
+    
