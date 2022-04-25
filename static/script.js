a130e9181c18c2a00e08704ee30f2a803814e988
IF <illesf16@gmail.com>
2022-04-25 03:19:26 +0200

    Results returned by the server are processed by JS and displayed on the page.

--- static/script.js
+++ static/script.js
@@ -1,6 +1,5 @@
 
 button = document.querySelector("#calc")
-console.log(button)
 button.addEventListener("click", fetchFibo)
 
 
@@ -9,5 +8,14 @@ function fetchFibo() {
     algorithm = document.querySelector("input[name=method]:checked").value
     fetch("/calculate?n="+n+"&method="+algorithm)
     .then( data => data.json())
-    .then( data => console.log(data))
+    .then( data => {
+        output_par = document.querySelector("div#output p")
+        if (data.err) {
+            output_par.innerHTML = data.err
+        } else {
+            output_par.innerHTML = `The ${data.n}<sup>th</sup> Fibonacci number has been calculated in ${data.sec} seconds. The result has ${data.digits} digits and its approximate value is ${data.float_rep}.<br>You can copy the exact number from here:`
+            output_area = document.querySelector("div#output input")
+            output_area.value = data.fib
+        }
+     })
 }
