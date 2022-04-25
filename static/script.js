35a7c7d020f888bcb105cf431bfeac1e0d91f078
IF <illesf16@gmail.com>
2022-04-26 01:34:20 +0200

    Bugfix: output area updated in case of error.

--- static/script.js
+++ static/script.js
@@ -10,11 +10,12 @@ function fetchFibo() {
     .then( data => data.json())
     .then( data => {
         output_par = document.querySelector("div#output p")
+        output_area = document.querySelector("div#output input")
         if (data.err) {
             output_par.innerHTML = data.err
+            output_area.value = "ERROR"
         } else {
-            output_par.innerHTML = `The ${data.n}<sup>th</sup> Fibonacci number has been calculated in ${data.sec} seconds. The result has ${data.digits} digits and its approximate value is ${data.float_rep}.<br>You can copy the exact number from here:`
-            output_area = document.querySelector("div#output input")
+            output_par.innerHTML = `The ${data.n}<sup>th</sup> Fibonacci number has been calculated in ${data.sec} seconds. The result has ${data.digits} digits and its approximate value is ${data.float_rep}.<br>You can copy the exact number from the box below:`
             output_area.value = data.fib
         }
      })
